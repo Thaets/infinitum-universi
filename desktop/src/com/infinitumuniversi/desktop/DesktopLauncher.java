b1fca7e6803493c7d68f4c8fa9580ef74de32588
ThisIsAreku <alexandre.boucey@gmail.com>
2014-11-21 11:17:42 +0100

    First commit

---
+++ desktop/src/com/infinitumuniversi/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.infinitumuniversi.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.infinitumuniversi.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
